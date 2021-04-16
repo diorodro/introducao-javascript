465eed9f6e106a1bb3dc03ef3f45ee762dbe5f76
Rodrigo Dionizio <rodrigosantiagodionizio@hotmail.com>
2021-04-16 03:51:12 -0300

    mostrar na tela peso, ou altura, invalido

--- js/principal.js
+++ js/principal.js
@@ -3,5 +3,29 @@ titulo.textContent = "Aparecida Nutricionista"
 
 var paciente = document.querySelector("#primeiro-paciente")
 var tdPeso = paciente.querySelector(".info-peso")
+var tdAltura = paciente.querySelector(".info-altura")
 
-console.log(tdPeso)
+var peso = tdPeso.textContent
+var altura = tdAltura.textContent
+
+
+var tdImc = paciente.querySelector(".info-imc")
+var pesoValido = true
+var alturaValida = true
+
+if (peso < 0 || peso > 450) {
+    console.log("Peso inválido!")
+    pesoValido = false
+    tdImc.textContent = "Peso inválido"
+}
+
+if (altura <= 0 || altura > 3) {
+    console.log("Altura inválida!")
+    alturaValida = false
+    tdImc.textContent = "Altura inválida"
+}
+
+if (pesoValido && alturaValida) {
+    var IMC = peso / (altura * altura)
+    tdImc.textContent = IMC
+}
