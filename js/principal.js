f3f36f9d3beef6072f928702cfe89b7916ecf4ee
Rodrigo Dionizio <rodrigosantiagodionizio@hotmail.com>
2021-04-17 01:06:29 -0300

    Mudando no estilo da página

--- js/principal.js
+++ js/principal.js
@@ -1,31 +1,40 @@
 var titulo = document.querySelector(".titulo")
 titulo.textContent = "Aparecida Nutricionista"
 
-var paciente = document.querySelector("#primeiro-paciente")
-var tdPeso = paciente.querySelector(".info-peso")
-var tdAltura = paciente.querySelector(".info-altura")
-
-var peso = tdPeso.textContent
-var altura = tdAltura.textContent
+var pacientes = document.querySelectorAll(".paciente")
 
 
-var tdImc = paciente.querySelector(".info-imc")
-var pesoValido = true
-var alturaValida = true
+for (var i = 0; i < pacientes.length; i++) {
+    var tdPeso = pacientes[i].querySelector(".info-peso")
+    var tdAltura = pacientes[i].querySelector(".info-altura")
 
-if (peso <= 0 || peso > 450) {
-    console.log("Peso inválido!")
-    pesoValido = false
-    tdImc.textContent = "Peso inválido"
-}
+    var peso = tdPeso.textContent
+    var altura = tdAltura.textContent
 
-if (altura <= 0 || altura >= 3) {
-    console.log("Altura inválida!")
-    alturaValida = false
-    tdImc.textContent = "Altura inválida"
-}
 
-if (pesoValido && alturaValida) {
-    var IMC = peso / (altura * altura)
-    tdImc.textContent = IMC
+    var tdImc = pacientes[i].querySelector(".info-imc")
+    var pesoValido = true
+    var alturaValida = true
+
+    if (peso <= 0 || peso > 450) {
+        console.log("Peso inválido!")
+        pesoValido = false
+        tdImc.textContent = "Peso inválido"
+        pacientes[i].style.color = "yellow"
+        pacientes[i].style.backgroundColor = "purple"
+    }
+
+    if (altura <= 0 || altura >= 3) {
+        console.log("Altura inválida!")
+        alturaValida = false
+        tdImc.textContent = "Altura inválida"
+        pacientes[i].style.color = "whitesmoke"
+        pacientes[i].classList.add("paciente-invalido")
+    }
+
+    if (pesoValido && alturaValida) {
+        var IMC = peso / (altura * altura)
+        tdImc.textContent = IMC.toFixed(2)
+    }
+
 }
